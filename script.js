a2901ad186add5e5353b5abda987cf1b01546eab
le <lettyciasilvag@hotmail.com>
2025-09-16 16:41:20 -0300

    Primeiro commit do projeto ONG Adote

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+// Mostrar alerta de envio nos formulários
+document.addEventListener("DOMContentLoaded", () => {
+    const forms = document.querySelectorAll("form");
+
+    forms.forEach((form) => {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault(); // Evita o envio real
+
+            alert("Formulário enviado com sucesso! Obrigado pelo contato 😊");
+
+            form.reset(); // Limpa os campos
+        });
+    });
+});
